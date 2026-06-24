73e6e01989e58cb4af87adc3a91109431c48b743
Swellen Azevedo <swazevedo38@gmail.com>
2026-06-24 01:30:39 -0300

    Implement basic arithmetic operations in C calculator

--- linguagem-c/calculadora-simples.c
+++ linguagem-c/calculadora-simples.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main(){
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
 
+    // Operação soma, subtração, multiplicação e divisão.
+    printf("Entre com o primeiro número: \n");
+    scanf("%d", &numero1);
+    printf("Entre com o segundo número: \n");
+    scanf("%d", &numero2);
+
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = numero1 / numero2;
+
+    printf("A soma é: %d\n", soma);
+    printf("A subtração é: %d\n", subtracao);
+    printf("A multiplicação é: %d\n", multiplicacao);
+    printf("A divisão é: %d\n", divisao);
+
+    return 0;
+}
